cdfaf92a746b5d8140b6159e1e53c1971f85aae0
Alex Pritchard <pritchard.alex@gmail.com>
2020-01-04 17:48:09 -0600

    Added test controller to verify installation of starter code

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/CritterController.java
@@ -0,0 +1,17 @@
+package com.udacity.jdnd.course3.critter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Dummy controller class to verify installation success. Do not use for
+ * your project work.
+ */
+@RestController
+public class CritterController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Success";
+    }
+}
